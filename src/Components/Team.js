4b9c6e821c2d4c54e274756fc1895101e30f9e54
Sarthak Luthra <57385445+SarthakLuthra062@users.noreply.github.com>
2021-11-10 20:52:40 +0530

    Fix Update

--- src/Components/Team.js
+++ src/Components/Team.js
@@ -1,69 +1,45 @@
 import React from "react";
-import mohammed from "../assets/mohammed.png"
-import qa from "../assets/qa.png"
-import marcus from "../assets/marcus.png"
-import quan from "../assets/quan.png"
+import mohammed from "../assets/mo.png"
+import qa from "../assets/vol.png"
+import marcus from "../assets/ma.png"
+import quan from "../assets/quang.png"
 import linkedin from "../assets/linkedin.png"
 
 
 function Team() {
   return (
-    < div style={{display:'flex',flexWrap:'wrap',justifyContent:'center',margin:'0 10%',padding:"100px 10px"}}
-    >
-
-<div>
-  <img src={mohammed} alt="mohammed" style={{ width: "15vw" }}/>
-
-
-  <a href="default.asp">
-<img src={linkedin} alt="mohammed" style={{ width: "3vw", color:"black",position:"relative",top:"8vw",right:"8vw"  }}/>
-</a>
-<div style={{fontSize:"22px"}}>MOHAMMED SOHEL SHAIKH </div>
-
-<div style={{fontSize:"25px"}}> CEO</div>
+    
+    <div>
+      <p style={{color:"orange",fontSize:"50px", margin:"10px 0"}}>OUR TEAM</p>
+      < div style={{display:'flex',flexWrap:'wrap',justifyContent:'center',margin:'0 5%',padding:"40px 10px"}}>
+<div style={{width:"300px"}}>
+  <img src={mohammed} alt="mohammed" style={{ width: "15vw", alignContent: "center" }}/>
+  <p style={{fontSize:"22px"}}>MOHAMMED SOHEL SHAIKH</p>
+  <p style={{fontSize:"25px"}}>CEO</p>
+  <a href="https://www.linkedin.com/in/mohammad-sohel-shaikh-225989221/"><img src={linkedin} alt="mohammed" style={{ width: "3vw"}}/></a>
 </div>
 
-
-<div>
-
-<img src={quan} alt="mohammed" style={{ width: "15vw" }}/>
-
-<a href="default.asp">
-<img src={linkedin} alt="mohammed" style={{ width: "3vw", color:"black",position:"relative",top:"9vw",right:"8vw"  }}/>
-</a>
-<div style={{fontSize:"22px"}}>QUANG </div>
-
-<div style={{fontSize:"25px"}}> MARKETING HEAD
-</div>
+<div style={{width:"300px"}}>
+  <img src={qa} alt="mohammed" style={{ width: "15vw" }}/>
+  <p style={{fontSize:"22px"}}>VOLODYMYR DOBROVOLSKYI</p>
+  <p style={{fontSize:"25px"}}>QA ENGINEER</p>
+  <a href="https://www.linkedin.com/in/vova-dobrovolskiy/?originalSubdomain=ua"><img src={linkedin} alt="mohammed" style={{ width: "3vw"}}/></a>
 </div>
 
-
-<div>
-<img src={qa} alt="mohammed" style={{ width: "15vw" }}/>
-
-<a href="default.asp">
-<img src={linkedin} alt="mohammed" style={{ width: "3vw", color:"black",position:"relative",top:"8vw",right:"8vw"  }}/>
-</a>
-<div style={{fontSize:"22px"}}>VOLODYMYR DOBROVOLSKYI </div>
-
-<div style={{fontSize:"25px"}}> QA ENGINEER
-</div>
+<div style={{width:"300px"}}>
+  <img src={marcus} alt="mohammed" style={{ width: "15vw" }}/>
+  <p style={{fontSize:"22px"}}>MARCUS DSOUZA</p>
+  <p style={{fontSize:"25px"}}>SOFTWARE DEVELOPER</p>
+  <a href="https://www.linkedin.com/m/in/marcus-dsouza-17b178222/"><img src={linkedin} alt="mohammed" style={{ width: "3vw"}}/></a>
 </div>
 
-<div>
-<img src={marcus} alt="mohammed" style={{ width: "15vw" }}/>
-
-<a href="default.asp">
-<img src={linkedin} alt="mohammed" style={{ width: "3vw", color:"black",position:"relative",top:"8vw",right:"8vw"  }}/>
-</a>
-<div style={{fontSize:"22px"}}>
-MARCUS DSOUZA </div>
-
-<div style={{fontSize:"25px"}}>SOFTWARE DEVELOPER
-
+<div style={{width:"300px"}}>
+  <img src={quan} alt="mohammed" style={{ width: "15vw" }}/>
+  <p style={{fontSize:"22px"}}>QUANG</p>
+  <p style={{fontSize:"25px"}}>MARKETING HEAD</p>
+  <a href="https://www.linkedin.com/in/le-quang-tuan-865244220/"><img src={linkedin} alt="mohammed" style={{ width: "3vw"}}/></a>
 </div>
 </div>
-
 </div>
   );
 }
